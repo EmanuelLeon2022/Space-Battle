955a8523e055ef3387c4d63ac026521a23872d64
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-11-02 15:38:38 -0400

    started making classes

---
+++ script.js
@@ -0,0 +1,41 @@
+// create player classes
+ class pilot {
+    constructor(){
+        this.player = ''
+        this.hull =  20;
+        this.firepower = 5
+        this.accuracy = .7
+    }
+    bigShot(){
+
+    }
+    ugotShot(){
+
+    }
+}
+
+//created enemy class
+class enemy{
+    constructor(){
+        this.name =''
+        this.hull = 0
+        this.firepower = 0
+        this.accuracy = 0
+    }
+    eShot(){
+
+    }
+    egShot(){
+
+    }
+}
+
+const player1 = new pilot()
+
+/*if (attack alien)
+    then alien attack
+*/
+
+if (i<0){
+    return "Game Over"
+}
